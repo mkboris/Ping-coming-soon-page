b906637bd8708dea016927ac0300361e7e738eaa
makoge boris <makogeboris@gmail.com>
2024-01-09 10:32:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+document
+  .getElementById("emailForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("emailInput").value;
+    const errorMessage = document.getElementById("errorMessage");
+    const emailInput = document.getElementById("emailInput");
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      errorMessage.style.display = "block";
+      errorMessage.textContent = "Please enter a valid email address";
+      emailInput.classList.add("error-input");
+      emailInput.placeholder = "example@email.com";
+    } else {
+      errorMessage.style.display = "none";
+      errorMessage.textContent = "";
+      emailInput.classList.remove("error-input");
+      emailInput.placeholder = "Enter your email";
+      location.reload(); // Refreshes the page
+      alert("Thank you for subscribing");
+    }
+  });
